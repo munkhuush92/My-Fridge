d6b221303d37893cf42636da52d3e505c88bd454
Ian Nicholas <imnich91@gmail.com>
2017-03-10 19:37:09 -0800

    fixed dependencies error

--- app/src/main/java/iann91/uw/tacoma/edu/myfridge/Dashboard/DashboardActivity.java
+++ app/src/main/java/iann91/uw/tacoma/edu/myfridge/Dashboard/DashboardActivity.java
@@ -270,6 +270,10 @@ public class DashboardActivity extends AppCompatActivity
             fragment = new SearchRecipeFragment();
         } else if (id == R.id.grocery_dashboard) {
             fragment = new GroceryListFragment();
+            Bundle bundle = new Bundle();
+            bundle.putBoolean("List filled", mGrocListIsFilled);
+            bundle.putString("Grocery List", mGrocList);
+            fragment.setArguments(bundle);
         } else if (id == R.id.my_recipe_dash) {
             fragment = new MyRecipesFragment();
         } else if (id == R.id.scanner_dashboard) {
@@ -277,7 +281,13 @@ public class DashboardActivity extends AppCompatActivity
         } else if (id == R.id.plan_week_dashboard) {
             fragment = new PlanWeekFragment();
         }  else if (id == R.id.logout_dash) {
-    }
+            mSharedPreferences.edit().putBoolean(getString(R.string.LOGGEDIN), false).commit();
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            Log.i("LOGGIN OUT ", "MIKE");
+            return true;
+        }
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.content_dashboard, fragment).addToBackStack(null).commit();
